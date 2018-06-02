34a078a4933a4255dc4ebdcd6abc947ff77e385c
Kev Mayo <kev@superherocheesecake.com>
2018-06-02 15:10:49 +0200

    removed gulp sourcemaps

--- tools/build/sass.js
+++ tools/build/sass.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 var path = require('path');
-var sourcemaps = require('gulp-sourcemaps');
 var sassGlob = require('gulp-sass-glob');
 
 module.exports = function(gulp, plugins, config) {
@@ -10,14 +9,10 @@ module.exports = function(gulp, plugins, config) {
         var dest = path.resolve(src, config.sass.dest);
         src = path.join(src, config.sass.src, config.sass.entry);
 
-        console.log("src : " + src);
-
         gulp.src(src)
             .pipe(sassGlob())
-            .pipe(sourcemaps.init())
             .pipe(plugins.plumber())
             .pipe(plugins.sass())
-            .pipe(sourcemaps.write())
             .pipe(plugins.autoprefixer({browsers: ['last 2 versions', 'ie 11', 'safari >= 8', 'ios 9', 'android 4']}))
             .pipe(gulp.dest(dest));
     };
