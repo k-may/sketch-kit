32a8c6f5031c759a43fa0c8a3b31445d7933fe64
Kev <kevmayo@gmail.com>
2021-12-22 13:41:38 +0100

    hmr options updated

--- app/run.js
+++ app/run.js
@@ -1,6 +1,4 @@
-/*var gulp = require('gulp');
 
-var plugins = require('gulp-load-plugins')();*/
 var path = require('path');
 var fs = require('fs');
 
@@ -17,32 +15,19 @@ class Run {
 
     async start() {
 
-        //browser-sync
-        /*var taskPath = path.resolve(__dirname, '../tools/build/sass');
-        const scripts = require(taskPath)(gulp, plugins, this._tasksConfig);
-        var sketchKitPath = path.resolve(process.cwd(), 'sketch-kit/');
-        var scssPath = sketchKitPath + '/' + this._tasksConfig.sass.src;
-
-        this.watch(scssPath, scripts);
-        scripts();
-
-        //sass
-        taskPath = path.resolve(__dirname, '../tools/build/serve');
-
-        const serve = require(taskPath)(this._tasksConfig);
-        serve();*/
-
         const publicDir = path.resolve(process.cwd(), 'sketch-kit');
-        console.log(publicDir);
         const server = await createServer({
             // any valid user config options, plus `mode` and `configFile`
             root: publicDir,
             server: {
-                port: 3001
+                port: 3001,
+                hmr : {
+                    protocol: 'ws',
+                    host : "localhost"
+                }
             }
         })
         await server.listen()
-
         server.printUrls()
 
     }
